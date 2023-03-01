a97458051b79fa43b29e20b77b02e87ac94e2e39
派了个萌 <pylogmon@outlook.com>
2023-03-01 15:29:43 +0800

    chore: 适配windows圆角与自定义窗口

--- src-tauri/src/shortcut.rs
+++ src-tauri/src/shortcut.rs
@@ -1,24 +1,27 @@
 use crate::config::CONFIG;
 use crate::APP;
-use tauri::{GlobalShortcutManager, Manager, WindowEvent};
+use tauri::{GlobalShortcutManager, Manager};
+#[cfg(target_os = "windows")]
+use window_shadows::set_shadow;
 
 // 失去焦点自动关闭窗口
-fn on_lose_focus(event: &WindowEvent) {
-    match event {
-        WindowEvent::Focused(v) => {
-            if !v {
-                let handle = APP.get().unwrap();
-                match handle.get_window("translator") {
-                    Some(window) => {
-                        window.close().unwrap();
-                    }
-                    None => {}
-                }
-            }
-        }
-        _ => {}
-    }
-}
+// Gnome 下存在焦点捕获失败bug，windows下拖动窗口会失去焦点
+// fn on_lose_focus(event: &WindowEvent) {
+//     match event {
+//         WindowEvent::Focused(v) => {
+//             if !v {
+//                 let handle = APP.get().unwrap();
+//                 match handle.get_window("translator") {
+//                     Some(window) => {
+//                         window.close().unwrap();
+//                     }
+//                     None => {}
+//                 }
+//             }
+//         }
+//         _ => {}
+//     }
+// }
 
 // 划词翻译
 fn translate() {
@@ -44,7 +47,11 @@ fn translate() {
             .title("Translator")
             .build()
             .unwrap();
-            window.on_window_event(on_lose_focus);
+            // Windows 下拖动窗口会失去焦点
+            // window.on_window_event(on_lose_focus);
+            // css圆角对windows无效，需要单独设置
+            #[cfg(target_os = "windows")]
+            set_shadow(&window, true).unwrap();
         }
     };
 }
@@ -57,7 +64,7 @@ fn persistent_window() {
             window.close().unwrap();
         }
         None => {
-            let _window = tauri::WindowBuilder::new(
+            let window = tauri::WindowBuilder::new(
                 handle,
                 "persistent",
                 tauri::WindowUrl::App("index_persistent.html".into()),
@@ -72,6 +79,10 @@ fn persistent_window() {
             .title("Translator")
             .build()
             .unwrap();
+
+            // css圆角对windows无效，需要单独设置
+            #[cfg(target_os = "windows")]
+            set_shadow(&window, true).unwrap();
         }
     };
 }
