e50ea4591aabcaa6dc56eb1981bf077329d09382
派了个萌 <pylogmon@outlook.com>
2023-03-08 09:12:12 +0800

    fix: 修复Windows支持

--- src-tauri/src/selection.rs
+++ src-tauri/src/selection.rs
@@ -13,14 +13,17 @@ pub fn get_selection_text() -> Result<String, String> {
 #[cfg(target_os = "windows")]
 #[tauri::command]
 pub fn get_selection_text() -> Result<String, String> {
-    use crate::APP;
+    use cli_clipboard::{ClipboardContext, ClipboardProvider};
     use rdev::{simulate, EventType, Key};
+    use std::{thread, time::Duration};
     _ = simulate(&EventType::KeyPress(Key::ControlLeft));
     _ = simulate(&EventType::KeyPress(Key::KeyC));
     _ = simulate(&EventType::KeyRelease(Key::KeyC));
     _ = simulate(&EventType::KeyRelease(Key::ControlLeft));
-    let clip_manager = APP.clipboard_manager();
-    match clip_manager.read_text() {
+
+    thread::sleep(Duration::from_millis(200));
+    let mut ctx: ClipboardContext = ClipboardProvider::new().unwrap();
+    match ctx.get_contents() {
         Ok(v) => return Ok(v),
         Err(e) => return Err(format!("剪切板读取出错{}", e.to_string())),
     }
