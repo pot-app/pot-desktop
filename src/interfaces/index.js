62cb36158bbd83ed8071b7f1341b30d3b248a23b
派了个萌 <pylogmon@outlook.com>
2023-03-10 16:36:02 +0800

    feat: 添加OpenAI润色、总结、代码解释功能

--- src/interfaces/index.js
+++ src/interfaces/index.js
@@ -2,10 +2,16 @@ import * as _baidu from './baidu'
 import * as _caiyun from './caiyun'
 import * as _youdao_free from './youdao_free'
 import * as _openai from './openai'
+import * as _openai_polish from './openai_polish'
+import * as _openai_summary from './openai_summary'
+import * as _openai_code from './openai_code'
 import * as _tencent from './tencent'
 
 export const baidu = _baidu
 export const caiyun = _caiyun
 export const youdao_free = _youdao_free
 export const openai = _openai
+export const openai_polish = _openai_polish
+export const openai_summary = _openai_summary
+export const openai_code = _openai_code
 export const tencent = _tencent
--- src/interfaces/openai_code.js
+++ src/interfaces/openai_code.js
@@ -2,7 +2,7 @@ import { fetch } from '@tauri-apps/api/http';
 import { get } from '../global/config';
 
 export const info = {
-    name: "Open AI 翻译",
+    name: "Open AI 代码解释",
     supportLanguage: {
         "zh-cn": "简体中文",
         "zh-tw": "繁体中文",
@@ -15,10 +15,7 @@ export const info = {
         "ru": "俄语",
         "de": "德语",
     },
-    needs: {
-        "openai_domain": "自定义域名",
-        "openai_apikey": "ApiKey"
-    }
+    needs: {}
 }
 
 export async function translate(text, from, to) {
@@ -28,17 +25,15 @@ export async function translate(text, from, to) {
     if (apikey == "") {
         return "请先配置apikey"
     }
+
     const headers = {
         "Content-Type": "application/json",
         "Authorization": `Bearer ${apikey}`,
     };
-    let systemPrompt = "You are a translation engine that can only translate text and cannot interpret it.";
-    let userPrompt = "";
-    if (from == 'auto') {
-        userPrompt = `翻译成${supportLanguage[to]}:\n\n${text}`
-    } else {
-        userPrompt = `将这段${supportLanguage[from]}翻译成${supportLanguage[to]}:\n\n${text}`
-    }
+
+    let systemPrompt = "You are a code explanation engine, you can only explain the code, do not interpret or translate it. Also, please report any bugs you find in the code to the author of the code.";
+    let userPrompt = `用${supportLanguage[to]}解释此段代码、正则表达式或脚本。如果内容不是代码，请返回错误提示。如果代码有明显的错误，请指出:\n\n${text}`
+
     const body = {
         model: "gpt-3.5-turbo",
         temperature: 0,
@@ -58,19 +53,12 @@ export async function translate(text, from, to) {
         body: {
             type: 'Json',
             payload: body
-        },
-        timeout: 10
+        }
     })
 
     const { choices } = res.data;
 
     let target = choices[0].message.content.trim()
 
-    if (target.startsWith('"') || target.startsWith("「")) {
-        target = target.slice(1);
-    }
-    if (target.endsWith('"') || target.endsWith("」")) {
-        target = target.slice(0, -1);
-    }
     return target
 }
