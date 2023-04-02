b1c88573629e2d8f3c7acc2573dfe153c157ad57
派了个萌 <pylogmon@outlook.com>
2023-04-02 17:57:28 +0800

    feat: 实现按键设置快捷键
    #25

--- src/windows/config/components/ShortCutConfig/index.jsx
+++ src/windows/config/components/ShortCutConfig/index.jsx
@@ -11,11 +11,26 @@ export const shortcutPersistentAtom = atom('');
 export default function ShortCutConfig() {
     const [shortcutTranslate, setShortcutTranslate] = useAtom(shortcutTranslateAtom);
     const [shortcutPersistent, setShortcutPersistent] = useAtom(shortcutPersistentAtom);
-
+    const supportKey = ["Control", 'Shift', 'Alt', 'Command', 'Meta', 'Option'];
     useEffect(() => {
         setShortcutTranslate(get('shortcut_translate') || '');
         setShortcutPersistent(get('shortcut_persistent') || '')
     }, []);
+
+    function keyDown(e, value, set) {
+        if (e.key.length == 1 || supportKey.includes(e.key)) {
+            if (value) {
+                set(value + '+' + e.key);
+            } else {
+                if (e.key.length != 1) {
+                    set(e.key);
+                }
+            }
+        }
+        if (e.keyCode == 8) {
+            set('');
+        }
+    }
 
     return (
         <ConfigList label="⌨快捷键⌨">
@@ -24,7 +39,8 @@ export default function ShortCutConfig() {
                     fullWidth
                     value={shortcutTranslate}
                     placeholder='eg: CommandOrControl+D'
-                    onChange={(e) => { setShortcutTranslate(e.target.value) }}
+                    onKeyDown={(e) => { keyDown(e, shortcutTranslate, setShortcutTranslate) }}
+                    onFocus={() => { setShortcutTranslate('') }}
                 />
             </ConfigItem>
             <ConfigItem label="独立翻译窗口">
@@ -32,7 +48,8 @@ export default function ShortCutConfig() {
                     fullWidth
                     placeholder='eg: CommandOrControl+Shift+D'
                     value={shortcutPersistent}
-                    onChange={(e) => { setShortcutPersistent(e.target.value) }}
+                    onKeyDown={(e) => { keyDown(e, shortcutPersistent, setShortcutPersistent) }}
+                    onFocus={() => { setShortcutPersistent('') }}
                 />
             </ConfigItem>
         </ConfigList>
