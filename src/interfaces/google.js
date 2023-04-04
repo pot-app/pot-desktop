3d00e6b39780f633bea60c3b8f55775c1659f805
派了个萌 <pylogmon@outlook.com>
2023-04-05 01:05:44 +0800

    feat: 优化Google翻译

--- src/interfaces/google.js
+++ src/interfaces/google.js
@@ -1,6 +1,5 @@
 import request from './utils/request';
 import { get } from '../windows/translator/main';
-import { searchWord } from "./utils/dict";
 
 // 此接口只支持英汉互译
 export const info = {
@@ -29,33 +28,58 @@ export async function translate(text, from, to) {
     if (!(from in supportLanguage) || !(to in supportLanguage)) {
         return '该接口不支持该语言'
     }
-    if (text.split(' ').length == 1) {
-        let target = await searchWord(text);
-        if (target !== '') {
-            return target
-        }
-    }
+
     let domain = get('google_proxy') ?? "translate.google.com";
     if (domain == '') {
         domain = "translate.google.com"
     }
 
     let proxy = get('proxy') ?? '';
-    let res = await request(`https://${domain}/translate_a/single`, {
+    let res = await request(`https://${domain}/translate_a/single?dt=at&dt=bd&dt=ex&dt=ld&dt=md&dt=qca&dt=rw&dt=rm&dt=ss&dt=t`, {
+        headers: { 'User-Agent': `Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36`, },
         query: {
-            client: "at",
+            client: 'gtx',
             sl: supportLanguage[from],
             tl: supportLanguage[to],
-            dt: "t",
-            q: text,
+            hl: supportLanguage[to],
+            ie: 'UTF-8',
+            oe: 'UTF-8',
+            otf: '1',
+            ssel: '0',
+            tsel: '0',
+            kc: '7',
+            q: text
         },
         proxy: proxy
     })
 
     let result = JSON.parse(res);
     let target = ""
+    // 词典模式
+    if (result[1]) {
+        for (let i of result[1]) {
+            // 词性
+            target = target + '【' + i[0] + '】\n【释义】';
+            for (let r of i[1]) {
+                target = target + r + ', ';
+            }
+            target = target + '\n【联想】\n';
+            for (let r of i[2]) {
+                target = target + r[0] + ':  ';
+                for (let j of r[1]) {
+                    target = target + j + ', ';
+                }
+                target = target + '\n';
+            }
+            target += '\n';
+        }
+        return target;
+    }
+    // 翻译模式
     for (let r of result[0]) {
-        target = target + r[0]
+        if (r[0]) {
+            target = target + r[0]
+        }
     }
     return target
 }
