27099b9ff646ba53c8c72c8636f2d678feed14c8
派了个萌 <pylogmon@outlook.com>
2023-04-09 21:52:13 +0800

    feat: 添加Esc关闭翻译窗口

--- src/windows/translator/main.jsx
+++ src/windows/translator/main.jsx
@@ -1,4 +1,5 @@
 import { atom, createStore, Provider } from "jotai";
+import { appWindow } from "@tauri-apps/api/window";
 import ReactDOM from "react-dom/client";
 import React from "react";
 import { readConfig } from "../../global/config";
@@ -21,7 +22,13 @@ readConfig().then(
       <React.StrictMode>
         <div data-tauri-drag-region className="titlebar" />
         <Provider store={configStore}>
-          <App />
+          <div tabIndex={0} onKeyDown={(e) => {
+            if (e.key == 'Escape') {
+              appWindow.close();
+            }
+          }}>
+            <App />
+          </div>
         </Provider>
       </React.StrictMode>
     );
