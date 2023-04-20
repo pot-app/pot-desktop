21134a680fb0abf645cfa9227be2802f85755fa0
派了个萌 <pylogmon@outlook.com>
2023-04-20 20:20:42 +0800

    feat: 读取app所在目录下的配置文件
    #43

--- src-tauri/src/config.rs
+++ src-tauri/src/config.rs
@@ -5,6 +5,7 @@ use dirs::config_dir;
 use std::sync::Mutex;
 use std::{fs, io::Read, path::PathBuf};
 use tauri::api::notification::Notification;
+use tauri::utils::platform::current_exe;
 use tauri::Manager;
 use toml::{Table, Value};
 
@@ -16,6 +17,15 @@ fn get_app_config_dir() -> PathBuf {
 }
 
 fn get_app_config_file() -> PathBuf {
+    let exe_path = current_exe().expect("Get Exe Path Failed");
+    let exe_dir_path = exe_path.parent().expect("Get Exe Dir Failed");
+    let mut exe_config_path = exe_dir_path.to_path_buf();
+    exe_config_path.push("config.toml");
+    exe_config_path = dunce::canonicalize(exe_config_path).unwrap();
+
+    if exe_config_path.exists() {
+        return exe_config_path;
+    }
     let mut app_config_file_path = get_app_config_dir();
     app_config_file_path.push("config.toml");
     app_config_file_path
