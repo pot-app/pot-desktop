de07f0679187221c3d4ec41c5d212bbc8f16d4df
派了个萌 <pylogmon@outlook.com>
2023-05-06 00:55:54 +0800

    update

--- updater/updater.mjs
+++ updater/updater.mjs
@@ -12,7 +12,7 @@ async function resolveUpdater() {
 
     const darwin_aarch64 = `https://github.com/Pylogmon/pot/releases/download/${version}/pot.aarch64.app.tar.gz`;
     const darwin_aarch64_sig = await getSignature(darwin_aarch64 + '.sig');
-    const darwin_x86_64 = `https://github.com/Pylogmon/pot/releases/download/${version}/pot.app.tar.gz`;
+    const darwin_x86_64 = `https://github.com/Pylogmon/pot/releases/download/${version}/pot.x86_64.app.tar.gz`;
     const darwin_x86_64_sig = await getSignature(darwin_x86_64 + '.sig');
     const linux_x86_64 = `https://github.com/Pylogmon/pot/releases/download/${version}/pot_${version}_amd64.AppImage.tar.gz`;
     const linux_x86_64_sig = await getSignature(linux_x86_64 + '.sig');
