e0a9563c147c5ee5e27812ca6ba5098a350ea4a9
Pylogmon <pylogmon@outlook.com>
2023-06-08 15:17:24 +0800

    fix: Start OCR on ocr window is open

--- src/windows/Ocr/components/ImageArea/index.jsx
+++ src/windows/Ocr/components/ImageArea/index.jsx
@@ -1,6 +1,7 @@
 import { appCacheDir, join } from '@tauri-apps/api/path';
 import { convertFileSrc } from '@tauri-apps/api/tauri';
 import { appWindow } from '@tauri-apps/api/window';
+import { listen } from '@tauri-apps/api/event';
 import { atom, useAtom } from 'jotai';
 import { Box } from '@mui/material';
 import React, { useEffect } from 'react';
@@ -11,16 +12,27 @@ export const imgUrlAtom = atom('');
 export default function ImageArea() {
     const [imgUrl, setImgUrl] = useAtom(imgUrlAtom);
 
-    useEffect(() => {
-        appCacheDir().then((appCacheDirPath) => {
-            join(appCacheDirPath, 'pot_screenshot_cut.png').then((filePath) => {
-                setImgUrl(convertFileSrc(filePath));
-                if (appWindow.label === 'ocr') {
-                    void appWindow.show();
-                    void appWindow.setFocus();
-                }
+    function load_img() {
+        setImgUrl('');
+        setTimeout(() => {
+            appCacheDir().then((appCacheDirPath) => {
+                join(appCacheDirPath, 'pot_screenshot_cut.png').then((filePath) => {
+                    setImgUrl(convertFileSrc(filePath));
+                    if (appWindow.label === 'ocr') {
+                        void appWindow.show();
+                        void appWindow.setFocus();
+                    }
+                });
             });
-        });
+        }, 50);
+    }
+
+    listen('ocr', (_) => {
+        load_img();
+    });
+
+    useEffect(() => {
+        load_img();
     }, []);
 
     return (
