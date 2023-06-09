7bdb3c49a875a98d569670d4039cca4d9bba486b
ccslykx <ccslykx@outlook.com>
2023-06-10 00:01:55 +0800

    feat: volcengine ocr support

--- src/interfaces_ocr/index.js
+++ src/interfaces_ocr/index.js
@@ -3,9 +3,11 @@ import * as _baidu from './baidu';
 import * as _baidu_accurate from './baidu_accurate';
 import * as _tencent from './tencent';
 import * as _tencent_accurate from './tencent_accurate';
+import * as _volcengine from './volcengine';
 
 export const tesseract = _tesseract;
 export const baidu = _baidu;
 export const baidu_accurate = _baidu_accurate;
 export const tencent = _tencent;
 export const tencent_accurate = _tencent_accurate;
+export const volcengine = _volcengine;
