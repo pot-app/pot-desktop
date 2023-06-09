be23a91107b7e86f2b64047153db7ba41d489b89
Pylogmon <pylogmon@outlook.com>
2023-06-09 21:29:14 +0800

    feat: Add Baidu OCR Accurate

--- src/interfaces_ocr/index.js
+++ src/interfaces_ocr/index.js
@@ -1,5 +1,7 @@
 import * as _tesseract from './tesseract';
 import * as _baidu from './baidu';
+import * as _baidu_accurate from './baidu_accurate';
 
 export const tesseract = _tesseract;
 export const baidu = _baidu;
+export const baidu_accurate = _baidu_accurate;
