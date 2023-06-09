c8d5337a2bb84d14e6b9b6b8970ab7cbabb6e1a8
Pylogmon <pylogmon@outlook.com>
2023-06-10 00:04:26 +0800

    feat: Add OCR Space Support

--- src/interfaces_ocr/index.js
+++ src/interfaces_ocr/index.js
@@ -3,9 +3,15 @@ import * as _baidu from './baidu';
 import * as _baidu_accurate from './baidu_accurate';
 import * as _tencent from './tencent';
 import * as _tencent_accurate from './tencent_accurate';
+import * as _spaceocr1 from './spaceocr1';
+import * as _spaceocr2 from './spaceocr2';
+import * as _spaceocr3 from './spaceocr3';
 
 export const tesseract = _tesseract;
 export const baidu = _baidu;
 export const baidu_accurate = _baidu_accurate;
 export const tencent = _tencent;
 export const tencent_accurate = _tencent_accurate;
+export const spaceocr1 = _spaceocr1;
+export const spaceocr2 = _spaceocr2;
+export const spaceocr3 = _spaceocr3;
---
+++ src/interfaces_ocr/spaceocr2.js
@@ -0,0 +1,80 @@
+import { readBinaryFile, BaseDirectory } from '@tauri-apps/api/fs';
+import { fetch } from '@tauri-apps/api/http';
+import { get } from '../windows/main';
+import { ocrID } from '../windows/Ocr/components/TextArea';
+
+export const info = {
+    name: 'spaceocr2',
+    supportLanguage: {},
+    needs: [
+        {
+            config_key: 'spaceocr2_apikey',
+            place_hold: '',
+        }
+    ],
+};
+
+export async function ocr(imgurl, lang, setText, id) {
+    const apikey = get('spaceocr2_apikey') ?? '';
+
+    if (apikey === '') {
+        throw 'Please configure client_id and client_secret';
+    }
+
+    const url = 'https://api.ocr.space/parse/image';
+
+
+    let canvas = document.createElement('CANVAS');
+    let ctx = canvas.getContext('2d');
+    let img = new Image;
+    img.src = imgurl;
+
+    let base64 = await new Promise((resolve, reject) => {
+        img.onload = () => {
+            img.crossOrigin = 'anonymous';
+            canvas.height = img.height;
+            canvas.width = img.width;
+            ctx.drawImage(img, 0, 0);
+            let base64 = canvas.toDataURL('image/png');
+            if (base64 === 'data:,') {
+            } else {
+                resolve(base64);
+            }
+        }
+        img.onerror = async (e) => {
+            let img = await readBinaryFile('pot_screenshot_cut.png', { dir: BaseDirectory.AppCache });
+            let base64 = window.btoa(String.fromCharCode(...img));
+            resolve('data:image/png;base64,' + base64);
+        };
+    });
+    const res = await fetch(url, {
+        method: 'POST',
+        headers: {
+            apikey,
+            'content-type': 'application/x-www-form-urlencoded'
+        },
+        body: {
+            type: 'Text',
+            payload: `base64Image=${encodeURIComponent(base64)}&OCREngine=2`
+        }
+    }
+    )
+    if (res.ok) {
+        let result = res.data;
+        if (result['ParsedResults']) {
+            let target = '';
+            for (let i of result['ParsedResults']) {
+                target += i['ParsedText']
+            }
+            if (id === ocrID || id === 'translate') {
+                setText(target.trim());
+            }
+        } else {
+            throw JSON.stringify(result)
+        }
+    } else {
+        if (id === ocrID || id === 'translate') {
+            throw `Http Request Error\nHttp Status: ${res.status}\n${JSON.stringify(res.data)}`;
+        }
+    }
+}
