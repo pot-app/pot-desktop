8888ad2f6a37220e9c6f0f3a878b1ac0563f68f4
Pylogmon <pylogmon@outlook.com>
2023-06-10 17:38:57 +0800

    feat: Add Simple Latex Support

--- src/interfaces_ocr/index.js
+++ src/interfaces_ocr/index.js
@@ -7,6 +7,7 @@ import * as _ocrspace1 from './ocrspace1';
 import * as _ocrspace2 from './ocrspace2';
 import * as _ocrspace3 from './ocrspace3';
 import * as _volcengine from './volcengine';
+import * as _simplelatex from './simplelatex';
 
 export const tesseract = _tesseract;
 export const baidu = _baidu;
@@ -17,3 +18,4 @@ export const ocrspace1 = _ocrspace1;
 export const ocrspace2 = _ocrspace2;
 export const ocrspace3 = _ocrspace3;
 export const volcengine = _volcengine;
+export const simplelatex = _simplelatex;
