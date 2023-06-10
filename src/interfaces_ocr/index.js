2e40ed4601346a69021ae1a0d3acdd99bc50da6a
派了个萌 <pylogmon@outlook.com>
2023-06-10 11:02:37 +0800

    feat: Add OCR Space Support (#204)
    * feat: Add OCR Space Support
    
    * refactor: Update Name

--- src/interfaces_ocr/index.js
+++ src/interfaces_ocr/index.js
@@ -3,6 +3,9 @@ import * as _baidu from './baidu';
 import * as _baidu_accurate from './baidu_accurate';
 import * as _tencent from './tencent';
 import * as _tencent_accurate from './tencent_accurate';
+import * as _ocrspace1 from './ocrspace1';
+import * as _ocrspace2 from './ocrspace2';
+import * as _ocrspace3 from './ocrspace3';
 import * as _volcengine from './volcengine';
 
 export const tesseract = _tesseract;
@@ -10,4 +13,7 @@ export const baidu = _baidu;
 export const baidu_accurate = _baidu_accurate;
 export const tencent = _tencent;
 export const tencent_accurate = _tencent_accurate;
+export const ocrspace1 = _ocrspace1;
+export const ocrspace2 = _ocrspace2;
+export const ocrspace3 = _ocrspace3;
 export const volcengine = _volcengine;
