76ed89acf38b8c70ceeb0dd5e44867884e21820d
Pylogmon <pylogmon@outlook.com>
2023-07-24 16:04:50 +0800

    ci: fix ci

--- build.sh
+++ build.sh
@@ -1,21 +1,3 @@
 pnpm install
 pnpm prettier --write .
-
-if [[ "${INPUT_TARGET}" == "i686-unknown-linux-gnu" ]]; then
-    export PKG_CONFIG_PATH=/usr/lib/x86_64-linux-gnu/pkgconfig/:$PKG_CONFIG_PATH
-    export PKG_CONFIG_SYSROOT_DIR=/
-elif [[ "${INPUT_TARGET}" == "aarch64-unknown-linux-gnu" ]];then
-    export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER=aarch64-linux-gnu-gcc
-    export CC_aarch64_unknown_linux_gnu=aarch64-linux-gnu-gcc
-    export CXX_aarch64_unknown_linux_gnu=aarch64-linux-gnu-g++
-    export PKG_CONFIG_PATH=/usr/lib/aarch64-linux-gnu/pkgconfig
-    export PKG_CONFIG_ALLOW_CROSS=1
-elif [[ "${INPUT_TARGET}" == "armv7-unknown-linux-gnueabihf" ]];then
-    export CARGO_TARGET_ARMV7_UNKNOWN_LINUX_GNUEABIHF_LINKER=arm-linux-gnueabihf-gcc
-    export CC_armv7_unknown_linux_gnueabihf=arm-linux-gnueabihf-gcc
-    export CXX_armv7_unknown_linux_gnueabihf=arm-linux-gnueabihf-g++
-    export PKG_CONFIG_PATH=/usr/lib/arm-linux-gnueabihf/pkgconfig
-    export PKG_CONFIG_ALLOW_CROSS=1
-fi
-
-pnpm tauri build
+pnpm tauri build --target $INPUT_TARGET
