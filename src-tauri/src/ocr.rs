3696c961e279587fe006ed24060985a46b49ddc0
Pylogmon <pylogmon@outlook.com>
2023-07-27 02:19:30 +0800

    feat: Add MacOS System OCR

--- src-tauri/src/ocr.rs
+++ src-tauri/src/ocr.rs
@@ -1,13 +1,12 @@
-use dirs::cache_dir;
-
 #[tauri::command(async)]
 #[cfg(target_os = "windows")]
-pub fn system_ocr(app_handle: tauri::AppHandle, lang: &str) -> Result<String, String> {
+pub fn system_ocr(app_handle: tauri::AppHandle) -> Result<String, String> {
+    use dirs::cache_dir;
     use win_ocr::ocr;
     let mut app_cache_dir_path = cache_dir().expect("Get Cache Dir Failed");
     app_cache_dir_path.push(&app_handle.config().tauri.bundle.identifier);
     app_cache_dir_path.push("pot_screenshot_cut.png");
-    println!("{lang}");
+
     match ocr(app_cache_dir_path.to_str().unwrap()) {
         Ok(v) => return Ok(v),
         Err(e) => return Err(e.to_string()),
@@ -16,8 +15,13 @@ pub fn system_ocr(app_handle: tauri::AppHandle, lang: &str) -> Result<String, St
 
 #[tauri::command]
 #[cfg(target_os = "macos")]
-pub fn system_ocr(app_handle: tauri::AppHandle, lang: &str) -> Result<String, String> {
-    Err("Not supported yet".to_string())
+pub fn system_ocr(app_handle: tauri::AppHandle) -> Result<String, String> {
+    use dirs::cache_dir;
+
+    let mut app_cache_dir_path = cache_dir().expect("Get Cache Dir Failed");
+    app_cache_dir_path.push(&app_handle.config().tauri.bundle.identifier);
+    app_cache_dir_path.push("pot_screenshot_cut.png");
+    Ok(app_cache_dir_path.to_str().unwrap().to_string())
 }
 
 #[tauri::command]
--- src/interfaces_ocr/system.js
+++ src/interfaces_ocr/system.js
@@ -1,6 +1,7 @@
 import { ocrID } from '../windows/Ocr/components/TextArea';
 import { ocr as tesseract } from './tesseract';
 import { invoke } from '@tauri-apps/api';
+import { Command } from '@tauri-apps/api/shell';
 
 export const info = {
     name: 'system',
@@ -10,11 +11,23 @@ export const info = {
 
 export async function ocr(base64, lang, setText, id) {
     const is_linux = await invoke('is_linux');
+    const is_macos = await invoke('is_macos');
     // Linux 没法做系统OCR，用Tesseract代替
     if (is_linux) {
         await tesseract(base64, lang, setText, id);
+    } else if (is_macos) {
+        const img_path = await invoke('system_ocr');
+        const command = Command.sidecar('sidecar/ocr', img_path);
+        const output = await command.execute();
+        if (!output.code) {
+            if (ocrID === id || id === 'translate') {
+                setText(output.stdout);
+            }
+        } else {
+            throw output.stderr;
+        }
     } else {
-        const result = await invoke('system_ocr', { lang });
+        const result = await invoke('system_ocr');
         if (ocrID === id || id === 'translate') {
             setText(result);
         }
