4e462a76d1ef09a30d99effe525a0775d5f5b02a
Pylogmon <pylogmon@outlook.com>
2023-07-29 17:57:31 +0800

    chore: Fix rpm name

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ pnpm install
 pnpm prettier --write .
 pnpm tauri build --target $INPUT_TARGET -b deb
 cd src-tauri/target/$INPUT_TARGET/release/bundle/deb
-alien -r ./pot*.deb
+alien -r -k ./pot*.deb
 
 rename '-' '_' ./pot*.rpm
 rename '-' 'pylogmon' ./pot*.rpm
