dff0d53d3a99e08423be4d129940a6d1d9092cb8
Pylogmon <pylogmon@outlook.com>
2023-07-30 13:28:27 +0800

    chore: remove rpm

--- build-for-linux/entrypoint.sh
+++ build-for-linux/entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-apt-get update
-apt-get install -y alien wget rename
-
 wget https://nodejs.org/dist/v19.8.1/node-v19.8.1-linux-x64.tar.xz
 tar -Jxvf ./node-v19.8.1-linux-x64.tar.xz
 export PATH=$(pwd)/node-v19.8.1-linux-x64/bin:$PATH
--- build.sh
+++ build.sh
@@ -2,9 +2,3 @@ pnpm install
 pnpm prettier --write .
 pnpm tauri build --target $INPUT_TARGET -b deb
 cd src-tauri/target/$INPUT_TARGET/release/bundle/deb
-alien -r -k ./pot*.deb
-
-rename '-' '_' ./pot*.rpm
-rename '-' 'pylogmon' ./pot*.rpm
-rename 'pylogmon1.' '_' ./pot*.rpm
-rename 'x86_64' 'amd64' ./pot*.rpm
