c8feda5370c45e65099893fd420015ebd79bc821
Pylogmon <pylogmon@outlook.com>
2023-08-14 10:31:08 +0800

    feat: Recognize Config

---
+++ src/services/recognize/tesseract/info.ts
@@ -0,0 +1,21 @@
+export const info = {
+    name: 'tesseract',
+};
+
+export enum Language {
+    auto = 'auto',
+    zh_cn = 'ZH',
+    zh_tw = 'ZH',
+    ja = 'JA',
+    en = 'EN',
+    ko = 'KO',
+    fr = 'FR',
+    es = 'ES',
+    ru = 'RU',
+    de = 'DE',
+    it = 'IT',
+    tr = 'TR',
+    pt = 'PT-PT',
+    pt_br = 'PT-BR',
+    id = 'ID',
+}
