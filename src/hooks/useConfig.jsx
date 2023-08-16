a6ba8dccd8546577f861e27d12f6e6fb7db8de87
Avan <layouwen@gmail.com>
2023-08-17 00:07:54 +0800

    refactor: useConfig hook

--- src/hooks/useConfig.jsx
+++ src/hooks/useConfig.jsx
@@ -1,31 +1,50 @@
-import { useState, useCallback, useEffect } from 'react';
-import { store } from '../utils/store';
+import { useCallback, useEffect } from 'react';
+import { store, setConfig } from '../utils/store';
+import { debounce } from '../utils';
+import { useGetState } from './useGetState.js';
 
-export const useConfig = (key, defaultValue) => {
-    const [property, updateProperty] = useState(defaultValue);
+export const useConfig = (key, defaultValue, options = {}) => {
+    // TIPS: 期望的返回结构
+    // const { sync = true } = options;
 
+    // TIPS: 兼容原逻辑
+    const { sync = false } = options;
+    const [property, _setProperty, getProperty] = useGetState();
+
+    const setPropertyDebounce = useCallback(
+        debounce((v) => setConfig(key, v, true)),
+        []
+    );
+
+    const setProperty = useCallback((v, forceSync = false) => {
+        _setProperty(v);
+
+        const isSync = forceSync || sync;
+        isSync && setPropertyDebounce(v);
+    }, []);
+
+    // init
     useEffect(() => {
         store.get(key).then((v) => {
             if (v === null) {
-                store.set(key, property);
-                store.save();
+                // 这里可能要考虑一下是否每次 null 就是覆盖, 不是的话, 可能 options 加多一个类似 overwrite: true/false
+                setProperty(defaultValue, true);
             } else {
-                updateProperty(v);
+                setProperty(v);
             }
         });
     }, []);
 
-    let timer = null;
-    const saveProperty = useCallback((v) => {
-        updateProperty(v);
-        if (timer) {
-            clearTimeout(timer);
-        }
-        timer = setTimeout(() => {
-            store.set(key, v);
-            store.save();
-        }, 500);
-    }, []);
+    // TIPS: 期望的返回结构
+    // return [property, setProperty, getProperty];
 
-    return [property, saveProperty, updateProperty];
+    // TIPS: 兼容原逻辑
+    return [
+        property,
+        (v) => {
+            setProperty(v, true);
+        },
+        setProperty,
+        getProperty,
+    ];
 };
--- src/utils/store.js
+++ src/utils/store.js
@@ -1,18 +1,21 @@
 import { Store } from 'tauri-plugin-store-api';
 import { appConfigDir, join } from '@tauri-apps/api/path';
-import { watch } from "tauri-plugin-fs-watch-api";
+import { watch } from 'tauri-plugin-fs-watch-api';
 import { invoke } from '@tauri-apps/api';
+
 export let store = new Store();
 
 export async function initStore() {
     const appConfigDirPath = await appConfigDir();
     const appConfigPath = await join(appConfigDirPath, 'config.json');
     store = new Store(appConfigPath);
-    const _ = await watch(
-        appConfigPath,
-        async () => {
-            await store.load();
-            await invoke('reload_store');
-        }
-    );
+    const _ = await watch(appConfigPath, async () => {
+        await store.load();
+        await invoke('reload_store');
+    });
+}
+
+export function setConfig(key, value, sync = false) {
+    store.set(key, value);
+    sync && store.save();
 }
