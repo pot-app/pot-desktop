90aab0397c2c158c174379f77b7585893fb123f5
Pylogmon <pylogmon@outlook.com>
2023-08-30 14:02:16 +0800

    build: fix build script

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 # pnpm install --resolution-only
 pnpm install
-sed -i "s/#openssl/openssl={version=\"0.10.55\",features=[\"vendore\"]}/g" src-tauri/Cargo.toml
+sed -i "s/#openssl/openssl={version=\"0.10\",features=[\"vendored\"]}/g" src-tauri/Cargo.toml
 pnpm tauri build --target $INPUT_TARGET -b deb
 cd src-tauri/target/$INPUT_TARGET/release/bundle/deb
