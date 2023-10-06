42aa97ade482c58c6e5a997e8a17b268b2b71394
Pylogmon <pylogmon@outlook.com>
2023-10-06 15:32:28 +0800

    refactor: Update Tesseract LangData Path

--- src/services/recognize/tesseract/index.jsx
+++ src/services/recognize/tesseract/index.jsx
@@ -7,7 +7,7 @@ export async function recognize(base64, language) {
     } = await Tesseract.recognize('data:image/png;base64,' + base64, language, {
         workerPath: '/worker.min.js',
         corePath: '/tesseract-core-simd-lstm.wasm.js',
-        langPath: 'https://pub-f6afb74f13c64cd89561b4714dca1c27.r2.dev',
+        langPath: 'https://tessdata.pot-app.com',
     });
     if (language === Language.zh_cn || language === Language.zh_tw) {
         return text.replaceAll(' ', '').trim();
