a6dc8b1888546987ef7fcd21c53ee86f7de051d2
Pylogmon <pylogmon@outlook.com>
2023-10-11 12:07:20 +0800

    ci: Add AppImage for x64 Linux

--- .github/actions/build.sh
+++ .github/actions/build.sh
@@ -1,5 +1,8 @@
 # pnpm install --resolution-only
 pnpm install
 sed -i "s/#openssl/openssl={version=\"0.10\",features=[\"vendored\"]}/g" src-tauri/Cargo.toml
-pnpm tauri build --target $INPUT_TARGET -b deb
-cd src-tauri/target/$INPUT_TARGET/release/bundle/deb
+if [ "$INPUT_TARGET" = "x86_64-unknown-linux-gnu" ]; then
+    pnpm tauri build --target $INPUT_TARGET
+else
+    pnpm tauri build --target $INPUT_TARGET -b deb
+fi
