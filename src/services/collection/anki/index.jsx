35b02ae88f654729f11cf3f5767befa57dfa1755
lihz <jzjlihz@gmail.com>
2023-11-29 21:28:23 +0800

    feature: add pronunciation symbol and voice to anki card (#606)
    Co-authored-by: lihz <lihz@matrix.localdomain>

--- src/services/collection/anki/index.jsx
+++ src/services/collection/anki/index.jsx
@@ -40,27 +40,68 @@ export async function collection(source, target, options = {}) {
         return result;
     }
 
+    function ankiPronunciation(target) {
+        let results = [];
+        if (typeof target !== 'object' || target.pronunciations === undefined) {
+            return results;
+        }
+        for (let i = 0; i < target.pronunciations.length; i++) {
+            let pronunciation = target.pronunciations[i];
+
+            let region = pronunciation.region;
+            let symbol = pronunciation.symbol;
+
+            // make pronunciation symbol readable
+            region = region ? `[${region}]` : '';
+            symbol = symbol[0] === '/' ? symbol : `/${symbol}/`;
+            let regionSymbol = `${region} ${symbol}`;
+
+            let audio;
+            if (pronunciation.voice) {
+                // step1: convert number array to Char String
+                // step2: convert Char String to base64
+                let voiceString = String.fromCharCode(...pronunciation.voice);
+                let voice = btoa(voiceString);
+
+                let filename = `${region}_${source}.mp3`;
+                let fields = [`Voice${i + 1}`];
+
+                audio = { data: voice, filename, fields };
+            }
+            results.push({ regionSymbol, audio });
+        }
+        return results;
+    }
+
     await ankiConnect('createDeck', 6, { deck: 'Pot' });
+
     await ankiConnect('createModel', 6, {
-        modelName: 'Pot Card',
-        inOrderFields: ['Front', 'Back'],
+        modelName: 'Pot Card 2',
+        inOrderFields: ['Front', 'Back', 'Symbol1', 'Voice1', 'Symbol2', 'Voice2'],
         isCloze: false,
         cardTemplates: [
             {
-                Name: 'Pot Card',
+                Name: 'Pot Card 2',
                 Front: '{{Front}}',
-                Back: '{{FrontSide}}<hr id=answer>{{Back}}',
+                Back: '{{FrontSide}}<br>{{Symbol1}} {{Voice1}}<br>{{Symbol2}} {{Voice2}}<hr id=answer>{{Back}}',
             },
         ],
     });
+
+    let pronunciations = ankiPronunciation(target);
     await ankiConnect('addNote', 6, {
         note: {
             deckName: 'Pot',
-            modelName: 'Pot Card',
+            modelName: 'Pot Card 2',
             fields: {
                 Front: source,
                 Back: ankiText(target),
+                Symbol1: pronunciations[0] && pronunciations[0].regionSymbol,
+                Symbol2: pronunciations[1] && pronunciations[1].regionSymbol,
             },
+            audio: pronunciations.map((pronunciation) => {
+                return pronunciation.audio;
+            }),
         },
     });
 }
