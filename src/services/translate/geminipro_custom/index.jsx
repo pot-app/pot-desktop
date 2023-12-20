bda4d8af59082d2851c02987d899b172cee7afec
Pylogmon <pylogmon@outlook.com>
2023-12-20 22:22:07 +0800

    fix: Remove Safety Block

--- src/services/translate/geminipro_custom/index.jsx
+++ src/services/translate/geminipro_custom/index.jsx
@@ -35,6 +35,24 @@ export async function translate(text, from, to, options = {}) {
     };
     let body = {
         contents: promptList,
+        safetySettings: [
+            {
+                category: 'HARM_CATEGORY_HARASSMENT',
+                threshold: 'BLOCK_NONE',
+            },
+            {
+                category: 'HARM_CATEGORY_HATE_SPEECH',
+                threshold: 'BLOCK_NONE',
+            },
+            {
+                category: 'HARM_CATEGORY_SEXUALLY_EXPLICIT',
+                threshold: 'BLOCK_NONE',
+            },
+            {
+                category: 'HARM_CATEGORY_DANGEROUS_CONTENT',
+                threshold: 'BLOCK_NONE',
+            },
+        ],
     };
 
     if (stream) {
