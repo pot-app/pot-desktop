4aa012e492d81564888b64738431433a85bf9e86
Pylogmon <pylogmon@outlook.com>
2023-12-29 17:59:27 +0800

    ci: Fix CI

--- .github/actions/build-for-more-arch/entrypoint.sh
+++ .github/actions/build-for-more-arch/entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs -o rustup-init && \
-chmod +x rustup-init && \
-./rustup-init -y --default-toolchain stable --target $INPUT_TOOLCHAIN && \
-rm rustup-init && \
+apt-get install -y curl wget
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs -o rustup-init
+chmod +x rustup-init
+./rustup-init -y --default-toolchain stable --target $INPUT_TOOLCHAIN
+rm rustup-init
 source $HOME/.cargo/env
 
 wget https://nodejs.org/dist/v19.8.1/node-v19.8.1-linux-x64.tar.xz
@@ -18,7 +20,7 @@ if [ "$INPUT_TARGET" = "aarch64-unknown-linux-gnu" ]; then
     sed -i 's/http:\/\/\(.*\).ubuntu.com\/ubuntu\//[arch=amd64,i386] http:\/\/\1.archive.ubuntu.com\/ubuntu\//g' /etc/apt/sources.list
     dpkg --add-architecture arm64
     apt-get update
-    apt-get install -y libncurses6:arm64 libtinfo6:arm64 linux-libc-dev:arm64 libncursesw6:arm64 libssl3:arm64 libcups2:arm64
+    apt-get install -y libncurses6:arm64 libtinfo6:arm64 linux-libc-dev:arm64 libncursesw6:arm64 libcups2:arm64
     apt-get install -y --no-install-recommends g++-aarch64-linux-gnu libc6-dev-arm64-cross libssl-dev:arm64 libwebkit2gtk-4.0-dev:arm64 libgtk-3-dev:arm64 patchelf:arm64 librsvg2-dev:arm64 libxdo-dev:arm64 libxcb1:arm64 libxrandr2:arm64 libdbus-1-3:arm64 libayatana-appindicator3-dev:arm64
     export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER=aarch64-linux-gnu-gcc
     export CC_aarch64_unknown_linux_gnu=aarch64-linux-gnu-gcc
@@ -30,7 +32,7 @@ elif [ "$INPUT_TARGET" = "armv7-unknown-linux-gnueabihf" ]; then
     sed -i 's/http:\/\/\(.*\).ubuntu.com\/ubuntu\//[arch=amd64,i386] http:\/\/\1.archive.ubuntu.com\/ubuntu\//g' /etc/apt/sources.list
     dpkg --add-architecture armhf
     apt-get update
-    apt-get install -y libncurses6:armhf libtinfo6:armhf linux-libc-dev:armhf libncursesw6:armhf libssl3:armhf libcups2:armhf
+    apt-get install -y libncurses6:armhf libtinfo6:armhf linux-libc-dev:armhf libncursesw6:armhf libcups2:armhf
     apt-get install -y --no-install-recommends g++-arm-linux-gnueabihf libc6-dev-armhf-cross libssl-dev:armhf libwebkit2gtk-4.0-dev:armhf libgtk-3-dev:armhf patchelf:armhf librsvg2-dev:armhf libxdo-dev:armhf libxcb1:armhf libxrandr2:armhf libdbus-1-3:armhf libayatana-appindicator3-dev:armhf
     export CARGO_TARGET_ARMV7_UNKNOWN_LINUX_GNUEABIHF_LINKER=arm-linux-gnueabihf-gcc
     export CC_armv7_unknown_linux_gnueabihf=arm-linux-gnueabihf-gcc
@@ -42,7 +44,7 @@ elif [ "$INPUT_TARGET" = "riscv64gc-unknown-linux-gnu" ]; then
     sed -i 's/http:\/\/\(.*\).ubuntu.com\/ubuntu\//[arch=amd64,i386] http:\/\/\1.archive.ubuntu.com\/ubuntu\//g' /etc/apt/sources.list
     dpkg --add-architecture riscv64
     apt-get update
-    apt-get install -y libncurses6:riscv64 libtinfo6:riscv64 linux-libc-dev:riscv64 libncursesw6:riscv64 libssl3:riscv64 libcups2:riscv64
+    apt-get install -y libncurses6:riscv64 libtinfo6:riscv64 linux-libc-dev:riscv64 libncursesw6:riscv64 libcups2:riscv64
     apt-get install -y --no-install-recommends g++-riscv64-linux-gnu libc6-dev-riscv64-cross libssl-dev:riscv64 libwebkit2gtk-4.0-dev:riscv64 libgtk-3-dev:riscv64 patchelf:riscv64 librsvg2-dev:riscv64 libxdo-dev:riscv64 libxcb1:riscv64 libxrandr2:riscv64 libdbus-1-3:riscv64 libayatana-appindicator3-dev:riscv64
     export CARGO_TARGET_RISCV64_UNKNOWN_LINUX_GNU_LINKER=riscv64-linux-gnu-gcc
     export CC_riscv64_unknown_linux_gnu=riscv64-linux-gnu-gcc
